eb9a73348c1ce599a59b3ec1bfb4c59e57689d52
594760 <594760@stud.hvl.no>
2021-08-30 23:51:22 +0200

    Oppgave B5 Ferdig med Spørsmål

--- Programsringslab 2/src/no/hvl/dat100/OppgaveB5.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveB5.java
@@ -1,9 +1,43 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+
 public class OppgaveB5 {
 	
 	public static void main(String[]args) {
 		
+		for (int i = 0; i < 2;) {
+			
+			String poengsumTxt = showInputDialog("Poengsum på prøve: ");
+			int poengsum = Integer.parseInt(poengsumTxt);
+			char karakter = 'G';
+			
+			if (poengsum < 40 && poengsum >= 0) {
+				karakter = 'F';
+			}
+			else if (poengsum >= 40 && poengsum < 50) {
+				karakter = 'E';
+			}
+			else if (poengsum >= 50 && poengsum < 60) {
+				karakter = 'D';
+			}
+			else if (poengsum >= 60 && poengsum < 80) {
+				karakter = 'C';
+			}
+			else if (poengsum >= 80 && poengsum < 90) {
+				karakter = 'B';
+			}
+			else if (poengsum >= 90 && poengsum <= 100){
+				karakter = 'A';
+			}
+			else {
+				showMessageDialog(null, "Poengsum " + poengsum + " er ugylidg.");
+			}
+			if (karakter != 'G') {
+				showMessageDialog(null, "Med poengsummen " + poengsum + " får du karakteren " + karakter);
+				i++;
+			}
+		}
 	}
 
 }
