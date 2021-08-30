24505b48a99f93de27764a0aecba1a8f1d6cffac
594760 <594760@stud.hvl.no>
2021-08-30 09:51:51 +0200

    en oppgave fra Proframeringslab 1

---
+++ Programeringslab 1/src/no/hvl/dat100/Vekslepenger.java
@@ -0,0 +1,23 @@
+package no.hvl.dat100;
+
+import static javax.swing.JOptionPane.*;
+
+public class Vekslepenger {
+
+	public static void main (String[] args) {
+		String prisTxt = showInputDialog("Pris:");
+		String betaltTxt = showInputDialog("Betalt:");
+		int pris = Integer.parseInt(prisTxt);
+		int betalt = Integer.parseInt(betaltTxt);
+		
+		int tiKroner = (betalt - pris)/10;
+		int enKroner = (betalt - pris)%10;
+		
+		if (tiKroner == 1){
+			showMessageDialog(null, "Du skal ha tilbake " + tiKroner + " ti korne og " + enKroner + " en kroner.");
+		}
+		else {
+			showMessageDialog(null, "Du skal ha tilbake " + tiKroner + " ti korner og " + enKroner + " en kroner.");			
+		}
+	}
+}
