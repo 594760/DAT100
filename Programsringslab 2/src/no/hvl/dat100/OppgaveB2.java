826c72424c16b9494cfb5b44464b668b50e2a043
594760 <594760@stud.hvl.no>
2021-08-30 22:55:43 +0200

    Oppgave B2 og B3 Ferdig

--- Programsringslab 2/src/no/hvl/dat100/OppgaveB2.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveB2.java
@@ -1,9 +1,24 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
 public class OppgaveB2 {
 	
 	public static void main(String[]args) {
+		String prisTxt = showInputDialog("Pris:");
+		String betaltTxt = showInputDialog("Betalt:");
+		int pris = Integer.parseInt(prisTxt);
+		int betalt = Integer.parseInt(betaltTxt);
 		
+		int tiKroner = (betalt - pris)/10;
+		int enKroner = (betalt - pris)%10;
+		
+		if (tiKroner == 1){
+			showMessageDialog(null, "Du skal ha tilbake " + tiKroner + " ti korne og " + enKroner + " en kroner.");
+		}
+		else {
+			showMessageDialog(null, "Du skal ha tilbake " + tiKroner + " ti korner og " + enKroner + " en kroner.");
+		}
 	}
-
 }
--- Programsringslab 2/src/no/hvl/dat100/OppgaveB3.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveB3.java
@@ -1,9 +1,32 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+
 public class OppgaveB3 {
 	
 	public static void main(String[]args) {
+		String brukernavn = showInputDialog("Brukernavn:");
 		
+		String passord = showInputDialog("Passord: ");
+		
+		String rettBrukernavn = "olav223";
+		
+		String rettPassord = "1234";
+		
+		if (brukernavn.equals(rettBrukernavn)) {
+			rettBrukernavn = "1";
+		}
+		if (passord.equals(rettPassord)) {
+			rettPassord = "1";
+		}
+		if (rettBrukernavn.equals("1") && rettPassord.equals("1")) {
+			showMessageDialog(null, "Velkommen inn!");
+		}
+		else {
+			showMessageDialog(null, "Brukernavn eller passord er feil!");
+		}
 	}
+	
+	
 
 }
