c4204d72a41fba3dd0c1bfe1fce40c1d96dfab96
594760 <594760@stud.hvl.no>
2021-08-31 11:15:32 +0200

    Oppgave B5

--- Programsringslab 2/src/no/hvl/dat100/OppgaveB5.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveB5.java
@@ -5,38 +5,38 @@ import static javax.swing.JOptionPane.*;
 public class OppgaveB5 {
 	
 	public static void main(String[]args) {
-		// Skal man skrive ut alle karakterene samtidig?
+		
 		for (int i = 0; i < 2;) {
 			
 			String poengsumTxt = showInputDialog("Poengsum på prøve: ");
 			int poengsum = Integer.parseInt(poengsumTxt);
-			char karakter = 'G';
+			char karakter = 'U';
 			
 			if (poengsum < 40 && poengsum >= 0) {
-				karakter = 'F';
-			}
-			else if (poengsum >= 40 && poengsum < 50) {
-				karakter = 'E';
-			}
-			else if (poengsum >= 50 && poengsum < 60) {
-				karakter = 'D';
-			}
-			else if (poengsum >= 60 && poengsum < 80) {
-				karakter = 'C';
-			}
-			else if (poengsum >= 80 && poengsum < 90) {
-				karakter = 'B';
-			}
-			else if (poengsum >= 90 && poengsum <= 100){
-				karakter = 'A';
-			}
+				karakter = 'F';}
+			
+			else if (poengsum < 50) {
+				karakter = 'E';}
+			
+			else if (poengsum < 60) {
+				karakter = 'D';}
+			
+			else if (poengsum < 80) {
+				karakter = 'C';}
+			
+			else if (poengsum < 90) {
+				karakter = 'B';}
+			
+			else if (poengsum <= 100){
+				karakter = 'A';}
+			
 			else {
-				showMessageDialog(null, "Poengsum " + poengsum + " er ugylidg.");
-			}
-			if (karakter != 'G') {
+				showMessageDialog(null, "Poengsum " + poengsum + " er ugylidg.");}
+			
+			if (karakter != 'U') {
 				showMessageDialog(null, "Med poengsummen " + poengsum + " får du karakteren " + karakter);
-				i++;
-			}
+				i++;}
+			
 		}
 	}
 
