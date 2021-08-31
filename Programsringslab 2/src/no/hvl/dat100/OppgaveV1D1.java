ca71f0a7e448fef173caba403f0f121192bfb0b2
594760 <594760@stud.hvl.no>
2021-08-31 13:08:56 +0200

    Den funker ikke med to like tall

--- Programsringslab 2/src/no/hvl/dat100/OppgaveV1D1.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveV1D1.java
@@ -5,7 +5,39 @@ import static javax.swing.JOptionPane.*;
 public class OppgaveV1D1 {
 	
 	public static void main(String[]args) {
+		String tall1Txt = showInputDialog("Første tall: ");
+		String tall2Txt = showInputDialog("Andre tall: ");
+		String tall3Txt = showInputDialog("Tredje tall: ");
 		
-	}
+		int tall1 = Integer.parseInt(tall1Txt);
+		int tall2 = Integer.parseInt(tall2Txt);
+		int tall3 = Integer.parseInt(tall3Txt);
+		
+		if (tall1 > tall2 && tall1 > tall3) {
+			if (tall2 > tall3) {
+				showMessageDialog(null, "Tallene i stigende rekkefølge: " + tall3 + ", " + tall2 + ", " + tall1);
+			}
+			if (tall3 > tall2) {
+				showMessageDialog(null, "Tallene i stigende rekkefølge: " + tall2 + ", " + tall3 + ", " + tall1);
+			}
+		}
+		if (tall2 > tall1 && tall2 > tall3) {
+			if (tall1 > tall3) {
+				showMessageDialog(null, "Tallene i stigende rekkefølge: " + tall3 + ", " + tall1 + ", " + tall2);			
+			}
+			if (tall3 > tall1) {
+				showMessageDialog(null, "Tallene i stigende rekkefølge: " + tall1 + ", " + tall2 + ", " + tall3);
+			}
+			
+		}	
+		if (tall3 > tall1 && tall3 > tall2) {
+			if (tall1 > tall2) {
+				showMessageDialog(null, "Tallene i stigende rekkefølge: " + tall2 + ", " + tall1 + ", " + tall3);			
+			}
+			if (tall2 > tall1) {
+				showMessageDialog(null, "Tallene i stigende rekkefølge: " + tall1 + ", " + tall2 + ", " + tall3);
+			}
+		}
 
+	}
 }
--- Programsringslab 2/src/no/hvl/dat100/OppgaveV1D2.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveV1D2.java
@@ -1,9 +1,19 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.showInputDialog;
+
 public class OppgaveV1D2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String tall1Txt = showInputDialog("Første tall: ");
+		String tall2Txt = showInputDialog("Andre tall: ");
+		String tall3Txt = showInputDialog("Tredje tall: ");
+		
+		int tall1 = Integer.parseInt(tall1Txt);
+		int tall2 = Integer.parseInt(tall2Txt);
+		int tall3 = Integer.parseInt(tall3Txt);
+		
+		
 
 	}
 
