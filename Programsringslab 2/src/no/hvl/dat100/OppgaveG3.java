d0c09f9d3fa5716f57d207a07db141a6ab6d87ce
594760 <594760@stud.hvl.no>
2021-09-01 15:06:32 +0200

    Rettelse i numerering av oppgavene og oppgradering av koden i G4

--- Programsringslab 2/src/no/hvl/dat100/OppgaveG3.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveG3.java
@@ -1,8 +1,18 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+
 public class OppgaveG3 {
 	
 	public static void main(String[]args) {
+		String aTxt = showInputDialog("Gi tall a: ");
+		String bTxt = showInputDialog("Gi tall B: ");
+		
+		int a = parseInt(aTxt);
+		int b = parseInt(bTxt);
+		
+		showMessageDialog(null, a/b);
 		
 	}
 
--- Programsringslab 2/src/no/hvl/dat100/OppgaveG4.java
+++ Programsringslab 2/src/no/hvl/dat100/OppgaveG4.java
@@ -1,18 +1,16 @@
 package no.hvl.dat100;
 
 import static javax.swing.JOptionPane.*;
-import static java.lang.Integer.*;
 
 public class OppgaveG4 {
 	
 	public static void main(String[]args) {
-		String aTxt = showInputDialog("Gi tall a: ");
-		String bTxt = showInputDialog("Gi tall B: ");
-		
-		int a = parseInt(aTxt);
-		int b = parseInt(bTxt);
-		
-		showMessageDialog(null, a/b);
+				
+		for (int i = 1; i < 6; i++) {
+			int tall  = Integer.parseInt(showInputDialog("Gi et tall: "));
+			showMessageDialog(null, "Tallet er: " + tall + " " + "Runde: " + i);
+			System.out.println(tall);
+		}
 		
 	}
 
