2e94da0a4f4879a81d3413deb40b8d10587c98f9
594760 <594760@stud.hvl.no>
2021-09-13 10:47:22 +0200

    komma må bort

--- Programeringslab 3/src/no/hvl/dat100/OppgaveB1.java
+++ Programeringslab 3/src/no/hvl/dat100/OppgaveB1.java
@@ -3,16 +3,36 @@ package no.hvl.dat100;
 import static java.lang.Integer.parseInt;
 import static javax.swing.JOptionPane.*;
 
+import sun.tools.jconsole.MaximizableInternalFrame;
+
 public class OppgaveB1 {
 	public static void main(String[] args) {
+		int lowerLimit = minNumber();
+		int upperLimit = maxNumber();
 		
-		int min = parseInt(showInputDialog("Nedre grense: "));
-		int max = parseInt(showInputDialog("Øvre grense: "));
-		if (min % 2 == 0) {
-			
+		String result = oddNumbers(lowerLimit, upperLimit);
+		
+		showMessageDialog(null, "Oddetalene er: " + result);
+	}
+	
+	private static int minNumber() {
+		int tall = parseInt(showInputDialog("Sett nedere grense: "));
+		return tall;
+	}
+	
+	private static int maxNumber() {
+		int tall = parseInt(showInputDialog("Sett øvre grense: "));
+		return tall;
+	}
+	
+	private static String oddNumbers(int startNumber, int endNumber) {
+		String numbersOdd= "";
+		for (int i = startNumber; i <= endNumber;) {
+			if (i % 2 == 1) {
+				numbersOdd = numbersOdd + ", " + String.valueOf(i); 
+			}
+			i++;
 		}
-		else {
-			
-		}
+		return numbersOdd;
 	}
 }
