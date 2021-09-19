8c8fc3510e6a8c3c4ddae25bbd71a8c600313e02
594760 <594760@stud.hvl.no>
2021-09-19 20:01:53 +0200

    yes

---
+++ Programeirngslab 5/src/no/hvl/dat100/OppgaveG1H.java
@@ -0,0 +1,42 @@
+package no.hvl.dat100;
+
+public class OppgaveG1H {
+	public static void main(String[] args) {
+		int[] tabell = {1, 3, 4, 7, 9, 10};
+		skrivUt(tabell);
+		System.out.println(finnes(tabell, 3));
+		int[] nyTabell = skifteForteng(tabell);
+		
+		for (int name: nyTabell)
+			System.out.println(name);
+	}	
+		
+				
+	public static void skrivUt(int [] tab) {
+		for (int name : tab) {
+			System.out.println(name);
+		}
+	}
+	
+	
+	public static boolean finnes(int [] tabell, int tall) {
+		int i = 0;
+		boolean finnesDet = false;
+		while (!finnesDet && i < tabell.length) {
+			if (tabell[i] == tall)
+				finnesDet = true;
+				i++;
+			}
+		return finnesDet;
+		}
+	public static int [] skifteForteng(int [] tabell) {
+		
+		int[] nyTabell = new int [tabell.length];
+		for (int i = 0; i < tabell.length; i++) {
+			nyTabell[i] = tabell[i] * (-1);
+		}
+		
+		
+		return nyTabell;
+	}
+	}
