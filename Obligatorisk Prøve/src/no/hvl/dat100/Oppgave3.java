72add2dab4ca3b8384e867fcec3f45662fe068bb
594760 <594760@stud.hvl.no>
2021-09-29 00:17:17 +0200

    29.09.2021

--- Obligatorisk Prøve/src/no/hvl/dat100/Oppgave3.java
+++ Obligatorisk Prøve/src/no/hvl/dat100/Oppgave3.java
@@ -8,7 +8,25 @@ public class Oppgave3 {
 		//Har importet:
 		//import static javax.swing.JOptionPane.*;
 		//import static java.lang.Integer.parseInt;
-
+		
+		int antall = 0;
+		int sum = 0;
+		int tall = 0;
+		
+		do {
+			String svar = showInputDialog("Tall? (Du avslutter med å skrive 0) ");
+			tall = parseInt(svar);
+			if (tall != 0) {
+				antall++;
+				sum += tall;
+			}
+		} while (tall != 0);
+		System.out.println("Antall tall: " + antall);
+		System.out.println("sum: " + sum);
+		
+		
+		//dette var det jeg gjorde på prøven:
+		/*
 		int antallTall = parseInt(showInputDialog("Hvor mange tall skal du ha: "));
 		int sum = 0;
 		for (int i = 0; i < antallTall; i++) {
@@ -17,6 +35,6 @@ public class Oppgave3 {
 		}
 		System.out.println("Antall tall: " + antallTall);
 		System.out.println("sum: " + sum);
-		
+		*/
 	}
 }
--- Programeringslab 6/src/no/hvl/dat100/OppgaveG1.java
+++ Programeringslab 6/src/no/hvl/dat100/OppgaveG1.java
@@ -1,78 +1,84 @@
 package no.hvl.dat100;
 
-
 public class OppgaveG1 {
 
 	public static void main(String[] args) {
-		double [][] obs = {
-				{1.4, 1.7, 1.9, 2.0},
-				{5.4, 7.8, 9.8, 7.9, 3.0},
-				{-1.7, -1.7, -2.8}
-		};
-		
+		double[][] obs = { { 1.4, 1.7, 1.9, 2.0 }, { 5.4, 7.8, 9.8, 7.9, 3.0 }, { -1.7, -1.7, -2.8 } };
+
 		skrivUtUFor(obs);
 		skrivUt(obs);
-		
-		System.out.println(obs.length);
-		System.out.println("Den skriver ut 3 fordi men obs.length sjekker han hvor mange tabeller det er i flerdimesjonaletabellen.");
-		System.out.println();
-		
-		System.out.println(obs[0].length);
-		System.out.println("Den skriver ut 4 fordi men obs[0].length sjekker han hvor mange elementer det er i den første tabellen i flerdimesjonaletabellen.");
-		System.out.println();
 
-		System.out.println(obs[1].length);
-		System.out.println("Den skriver ut 5 fordi men obs[1].length sjekker han hvor mange elementer det er i den andre tabellen i  flerdimesjonaletabellen.");
-		System.out.println();
+		oppgaveBogC(obs);
 
-		System.out.println(obs[2].length);
-		System.out.println("Den skriver ut 3 fordi men obs[2].length sjekker han hvor mange elementer det er i den tredje tabellen i flerdimesjonaletabellen.");
-		System.out.println();
-		
 		double limit = 3.3;
 		System.out.println(størreEnnLimit(obs, limit));
 		System.out.println();
-		
-		double [][] obs2 = rekkefølgeTabell(obs);
+
+		double[][] obs2 = rekkefølgeTabell(obs);
 		skrivUt(obs2);
 	}
-	public static void skrivUt(double [][] tabell) {
-		System.out.println("Skriver ut ved hjelp av for løkker");
-		for(int i = 0; i < tabell.length; i++) {
-			for(int j = 0; j < tabell[i].length; j++) {
+
+	public static void skrivUt(double[][] tabell) {
+		System.out.println("Skriver ut ved hjelp av for løkker:");
+		for (int i = 0; i < tabell.length; i++) {
+			for (int j = 0; j < tabell[i].length; j++) {
 				System.out.print(tabell[i][j] + "  ");
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
-	public static void skrivUtUFor(double [][] tabell) {
+
+		public static void skrivUtUFor(double[][] tabell) {
 		System.out.println("Skriver ut ved hjelp av Utividt For løkker:");
-		for (double [] i : tabell) {
-			for(double j : i) {
+		for (double[] i : tabell) {
+			for (double j : i) {
 				System.out.print(j + "  ");
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
-	
-	public static boolean størreEnnLimit(double [][] obs, double limit) {
-		for (double [] i : obs) {
-			for(double j : i) {
+		
+		public static void oppgaveBogC(double[][] obs) {
+			System.out.println(obs.length);
+			System.out.println(
+					"Den skriver ut 3 fordi men obs.length sjekker han hvor mange tabeller det er i flerdimesjonaletabellen.");
+			System.out.println();
+
+			System.out.println(obs[0].length);
+			System.out.println(
+					"Den skriver ut 4 fordi men obs[0].length sjekker han hvor mange elementer det er i den første tabellen i flerdimesjonaletabellen.");
+			System.out.println();
+
+			System.out.println(obs[1].length);
+			System.out.println(
+					"Den skriver ut 5 fordi men obs[1].length sjekker han hvor mange elementer det er i den andre tabellen i  flerdimesjonaletabellen.");
+			System.out.println();
+
+			System.out.println(obs[2].length);
+			System.out.println(
+					"Den skriver ut 3 fordi men obs[2].length sjekker han hvor mange elementer det er i den tredje tabellen i flerdimesjonaletabellen.");
+			System.out.println();
+		}
+
+
+	public static boolean størreEnnLimit(double[][] obs, double limit) {
+		for (double[] i : obs) {
+			for (double j : i) {
 				if (j > limit) {
 					return true;
 				}
-					
+
 			}
-		}	
+		}
 		return false;
 	}
-	 
-	public static double [][] rekkefølgeTabell(double [][] tab) {
-		double [][] nyTab = new double[tab.length][];
-		int j = tab.length-1;
-		for(int i = 0; i < tab.length; i++) {
+
+	public static double[][] rekkefølgeTabell(double[][] tab) {
+		double[][] nyTab = new double[tab.length][];
+		int j = tab.length - 1;
+		for (int i = 0; i < tab.length; i++) {
 			nyTab[i] = tab[j];
 			j--;
 		}
