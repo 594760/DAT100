094caa5bbdbaefbde0a9df033ff8578fc12ff239
594760 <594760@stud.hvl.no>
2021-09-29 00:50:19 +0200

    29.09.2021

---
+++ Programeringslab 6/src/no/hvl/dat100/OppgaveB2.java
@@ -0,0 +1,11 @@
+package no.hvl.dat100;
+
+public class OppgaveB2 {
+
+	public static void main(String[] args) {
+		int[][] terreng = { { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
+				{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 } };
+
+	}
+
+}
