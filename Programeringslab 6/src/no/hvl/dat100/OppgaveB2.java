462a0a846f1d22c4de25e602064a85601824d474
594760 <594760@stud.hvl.no>
2021-10-04 09:49:31 +0200

    v3

--- Programeringslab 6/src/no/hvl/dat100/OppgaveB2.java
+++ Programeringslab 6/src/no/hvl/dat100/OppgaveB2.java
@@ -1,11 +1,52 @@
 package no.hvl.dat100;
 
-public class OppgaveB2 {
+import easygraphics.EasyGraphics;
 
+public class OppgaveB2 extends EasyGraphics{
+
+	int[][] terreng = { { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
+			{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 } };
+	
 	public static void main(String[] args) {
-		int[][] terreng = { { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 }, { 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
-				{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 } };
-
+			launch(args);
 	}
-
+	
+	public void run() {
+		makeWindow("Flodding", 800,300);
+		
+		visulaiser();
+	}
+	
+	public void visulaiser() {
+		int x = 50;
+		int y = 50;
+		int radius = 30;
+		
+		System.out.println("Angi havhøyde i tegnevinduet ...");
+		int hav = Integer.parseInt(getText("Havhøyde: "));
+		
+		while (hav >= 0) {
+			for (int i  = 0; i < terreng.length; i++) {
+				for (int j = 0; j < terreng[i].length; j++) {
+					if(hav >= terreng[i][j]) {
+						setColor(0, 0, 255);
+					}
+					else if (hav+1 == terreng[i][j]) {
+						setColor(255, 140, 0);
+					}
+					else {
+						setColor(222, 184, 135);
+					}
+					
+					fillCircle(x, y, radius);
+					x = x+(2*radius);
+				}
+				x = 50;
+				y = y+(2*radius);
+			}
+			y = 50;
+			System.out.println("Angi havhøyde i tegnevinduet ...");
+			hav = Integer.parseInt(getText("Havhøyde: "));
+		}
+	}
 }
