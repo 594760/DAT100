fabce9bc2c6a368c5038e4c8a63d86b526174e5d
594760 <594760@stud.hvl.no>
2021-10-06 19:49:37 +0200

    06.10.2021

---
+++ P7oppgaveG2/src/no/hvl/dat100/Konto.java
@@ -0,0 +1,28 @@
+package no.hvl.dat100;
+
+public class Konto {
+	private int kontonr;
+	private double saldo;
+
+	public Konto(int kontonummer, double startSaldo) {
+		kontonr = kontonummer;
+		saldo = startSaldo;
+	}
+
+	public int getKontoNr() {
+		return kontonr;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public double sjekkeSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double beløp) {
+		saldo += beløp;
+	}
+
+}
---
+++ P7oppgaveG2/src/no/hvl/dat100/main.java
@@ -0,0 +1,10 @@
+package no.hvl.dat100;
+
+public class main {
+	public static void main(String[] args) {
+		Konto k1 = new Konto(2451, 35000);
+		Konto k2 = new Konto(2561, 23500);
+		Konto k3 = new Konto(2345, 6578.65);
+
+	}
+}
